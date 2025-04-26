38d320d2feb9657a658dcec93f8c641f97ca4231
SERGIO MASSO GIRALDO <sergio.masso@uao.edu.co>
2025-04-26 18:10:56 -0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Función para conectar a la base de datos de MongoDB
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://sergiomasso:4uauMDtMShZuMFt6@cluster0.ztucv.mongodb.net/login', {
+    await mongoose.connect('mongodb+srv:/url@cluster0.ztucv.mongodb.net/login', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
